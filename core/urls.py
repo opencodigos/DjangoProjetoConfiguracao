dce66a88545e2cda835920144130d86e0d7295b1
eticialima <leticialimacgi@gamil.com>
2025-10-07 00:00:20 -0300

    Atualiza Bootstrap

--- core/urls.py
+++ core/urls.py
@@ -21,10 +21,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
+    path('admin/', admin.site.urls), 
+    path('', include('myapp.urls')), # url do app
 ] 
 
+# Somente Quando Debug é True.
 if settings.DEBUG:  
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
